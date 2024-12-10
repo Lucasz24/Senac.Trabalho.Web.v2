d33da955e88eeeae1bfdca71dc526dce842e88d0
lufreitas <166768492+Lucasz24@users.noreply.github.com>
2024-12-09 21:59:23 -0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Senac.Trabalho.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contato()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
